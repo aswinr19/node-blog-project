0cf8d394cb90fac6a1fb68dbdd89eb793d9f9afe
aswinr <aswinr2442002@gmail.com>
2022-07-09 21:31:50 +0530

    added about , signup , signin view and corresponding route handlers

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ app.get('/',(req,res)=>{
     res.render('index',{title:'Home'});
 });
 
+app.get('/signin',(req,res)=>{
+    res.render('signin',{title:'Signin'});
+});
+
+app.post('/signin',(req,res)=>{
+  
+});
+
+app.get('/signup',(req,res)=>{
+    res.render('signup',{title:'Signup'});
+});
+
+app.post('/signup',(req,res)=>{
+   
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{title:'About Us'});
+});
+
+
+
 app.get('/blogs',(req,res)=>{
     res.render('blogs/index',{title:'Blogs'});
 });
@@ -37,5 +59,5 @@ app.get('/blogs/show/:id',(req,res)=>{
 
 
 app.use((req,res)=>{
-    res.staus(404).render('404',{title:'404'});
+    res.status(404).render('404',{title:'404'});
 });
