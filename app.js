4de7a21ee1313f21a5f9dd6aa797dba7379b555b
aswinr <aswinr2442002@gmail.com>
2022-07-09 20:41:25 +0530

    added blog views , routes to handle those views

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ app.get('/',(req,res)=>{
     res.render('index',{title:'Home'});
 });
 
+app.get('/blogs',(req,res)=>{
+    res.render('blogs/index',{title:'Blogs'});
+});
+
+app.get('/blogs/create-blog',(req,res)=>{
+    res.render('blogs/create',{title:'Create Blog'});
+});
+
+app.post('/blogs/create-blog',(req,res)=>{
+});
+
+app.get('/blogs/update-blog',(req,res)=>{
+    res.render('blogs/update',{title:'Update Blog'});
+});
+
+app.post('/blogs/update-blog',(req,res)=>{
+   
+});
+
+app.get('/blogs/show/:id',(req,res)=>{
+    res.render('blogs/show',{title:'Show Blog'});
+});
+
+
 app.use((req,res)=>{
-    res.render('404',{title:'404'});
+    res.staus(404).render('404',{title:'404'});
 });
