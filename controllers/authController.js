6a3c41a5cc0c22317dfd59f437d4eedf6331548b
aswinr <aswinr2442002@gmail.com>
2022-09-21 22:27:14 +0530

    signup not working

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,4 @@
 const User = require("../models/user");
-const jwt = require("jsonwebtoken");
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -21,14 +20,6 @@ const handleErrors = (err) => {
   return errors;
 };
 
-const maxAge = 3 * 24 * 60 * 60;
-
-const createToken = (id) => {
-  return jwt.sign({ id }, process.env.Secret, {
-    expiresIn: maxAge,
-  });
-};
-
 const userSignupGet = (req, res) => {
   res.render("auth/signup", { title: "Signup" });
 };
@@ -44,10 +35,7 @@ const userSignupPost = async (req, res) => {
       password,
     });
 
-    const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-
-    res.status(201).json({ user: user._id});
+    res.status(201).json(user);
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
